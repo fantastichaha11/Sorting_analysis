88370831430f4411d03e5a6fdba96595b29ea851
Nguyen Thanh Tuan Kiet <nttkiet22@clc.fitus.edu.vn>
2023-07-19 22:10:22 +0700

    test

--- pianoKiet.h
+++ pianoKiet.h
@@ -5,7 +5,10 @@
 #include <string>
 using namespace std;
 
+#define I_SORT 3
+#define SAKER_SORT 4
 //Insertion Sort
 void insertionSort_countCompare(int* a, int n, int& count_compare);
 void insertionSort_countTime(int* a, int n, int& count_compare);
+void shakerSort_countCompare(int* a, int n, int& count_compare);
 void shakerSort_countTime(int* a, int n, int& count_compare);
