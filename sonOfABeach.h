a61430325dceb55fddcc0778a62848dc66baae73
admin <22127368@student.hcmus.edu.vn>
2023-07-19 17:10:12 +0700

    update sonofabeach header

--- sonOfABeach.h
+++ sonOfABeach.h
@@ -1,1 +1,14 @@
 #pragma once
+#include<string>
+using namespace std;
+
+#define S_SORT 0
+#define SS_SORT 4
+#define C_SORT 8
+
+void selectionSort_countCompare(int* a, int n, int& count_compare);
+void selectionSort_countTime(int* a, int n, double& time);
+void countingSort_countCompare(int* a, int n, int k, int& count_compare);
+void countingSort_countTime(int* a, int n, int k, double& time);
+void shellSort_countCompare(int* a, int n, int& count_compare);
+void shellSort_countTime(int* a, int n, double& time);
