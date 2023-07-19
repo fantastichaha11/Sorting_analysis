b54fb95d325ecf06b75c22899deeaa79343dc1ec
MinhAn <MinhAn@DESKTOP-G3003B5>
2023-07-19 23:03:16 +0700

    change variable name

--- hieuHo.h
+++ hieuHo.h
@@ -1,11 +1,11 @@
 #pragma once
 void insertionSort(int a[], int n);
 void flashSort_time(int a[], int n, double& time);
-void insertionSort_compare(int a[], int n, int& countCmp);
-void flashSort_compare(int a[], int n, int& countCmp);
+void insertionSort_compare(int a[], int n, long long& count_compare);
+void flashSort_compare(int a[], int n, long long& count_compare);
 int getMax(int arr[], int n);
 void countSort(int arr[], int n, int exp);
 void radixSort_time(int arr[], int n, int& time);
-int getMax_compare(int arr[], int n, int& countCmp);
-void countSort_compare(int arr[], int n, int exp, int& countCmp);
-void radixSort_compare(int arr[], int n, int& countCmp);
+int getMax_compare(int arr[], int n, long long& count_compare);
+void countSort_compare(int arr[], int n, int exp, long long& count_compare);
+void radixSort_compare(int arr[], int n, long long& count_compare);
