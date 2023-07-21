bd8300be0d97597de8a2ca4b93731bff349c32f1
Nguyen Thanh Tuan Kiet <124020170+pianokill@users.noreply.github.com>
2023-07-22 00:25:12 +0700

    Update Function.h

--- Function.h
+++ Function.h
@@ -1,14 +1,10 @@
 #pragma once
 #include "sorting_functions.h"
 
-<<<<<<< HEAD
 void outputSort(int n, int typeData, int typeCount, int typeSort, string filename);
-=======
 typedef void(*pSort)(int*, int, long long&, double&, int);
 pSort listSort[11] = {selectionSort, insertionSort, bubbleSort, shakerSort, shellSort, heapSort,
 						mergeSort, quickSort, countingSort, radixSort, flashSort };
-
 void outputSort(int n, int typeData, int typeCount, int typeSort, string filename);
 void duplicateArr(int* a, int* b, int n);
 void outputSort_kiet(int typeData, string filename);
->>>>>>> output_kiet updated
