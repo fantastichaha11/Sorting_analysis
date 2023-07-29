b09dc4d17fdda2421ffb903d266efd81801d7d39
MinhAn <MinhAn@DESKTOP-G3003B5>
2023-07-29 16:55:33 +0700

    kcj

---
+++ Function.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "sorting_functions.h"
+
+#define A_MODE 0
+#define C_MODE 1
+
+void outputSort(int n, int typeData, int typeCount, int typeSort, string filename);
+void duplicateArr(int* src, int* des, int n);
+void outputSort_kiet(int typeData, string filename);
+void outputSort(int n, int typeData, int typeCount, int typeSort, string filename);
+void printCMD4(int algo1, int algo2, string var1, int* arr, int n);
+void printCMD5(int algo1, int algo2, string var2, int* arr, int n);
+
+bool KeySearch(string key, string arr[], int n, short& code);
+bool readInputFile(string fileName, int*& a, int& n);
+void printResult(short orderCode, short outputCode, double time, long long count_compare);
+void printCMD1(short algoCode, string fileName, short outputCode);
+void printCMD2(short algoCode, int inputSize, short orderCode, short outputCode);
+void printCMD3(short algoCode, int inputSize, short outputCode);
